429cc90dc4a50ded9ae8d2f869c240486dcf4c1b
notdevin <devinjelliot@gmail.com>
2024-08-25 15:37:51 -0700

    export types for sanity

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from './api';
 export * from './hooks';
 export * from './_types';
+export * from './api/_types/auth';
+export * from './api/_types/bridge';
+export * from './api/_types/merchant';
+export * from './api/_types/transaction';
